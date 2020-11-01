7b3f8d2366f01a23ad7d454dd18599b43404be2c
Kamil Stecyk <kamils987@interia.eu>
2020-11-02 00:39:24 +0100

    trzeci commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+//implikacja
+
+char a,b;
+if(a==’1’&& b=='0'){return 0;}
+else{return 1;}
+
+//alternatywa wykluczajaca
+
+if(a==0 && b==0){return 0;}
+else if(a==1 && b==1){return 0;}
+else{return 1;}
